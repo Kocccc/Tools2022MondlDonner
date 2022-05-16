9ec1aa115102c51a141ac03e8513c9f701e4eff5
mDonner <s2110237003@students.fh-hagenberg.at>
2022-05-16 17:09:42 +0200

    added method call

--- mondl_donner_android/src/DummyClass.java
+++ mondl_donner_android/src/DummyClass.java
@@ -2,6 +2,7 @@ public class DummyClass {
     public static void main(String[] args) {
         System.out.println("Astolfos Dummy Thicc Thighs");
 	System.out.println("Yummy");
+        thisIsANewMethod();
     }
 
     public void thisIsANewMethod(){
