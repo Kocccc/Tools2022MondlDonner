c8fddada0d999986854f24a4a274aafe636bb5f7
mDonner <s2110237003@students.fh-hagenberg.at>
2022-05-16 16:33:17 +0200

    added a new Method

--- mondl_donner_android/src/DummyClass.java
+++ mondl_donner_android/src/DummyClass.java
@@ -2,4 +2,8 @@ public class DummyClass {
     public static void main(String[] args) {
         System.out.println("Dummy Thicc");
     }
+
+    public void thisIsANewMethod(){
+        // sus
+    }
 }
