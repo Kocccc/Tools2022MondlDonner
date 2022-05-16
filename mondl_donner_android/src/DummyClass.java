a99966be0756955959e48a01508f2c2b6a4a9d63
mDonner <s2110237003@students.fh-hagenberg.at>
2022-05-16 17:11:52 +0200

    added comments to code

--- mondl_donner_android/src/DummyClass.java
+++ mondl_donner_android/src/DummyClass.java
@@ -3,12 +3,13 @@ public class DummyClass {
         System.out.println("Astolfos Dummy Thicc Thighs");
 	System.out.println("Yummy");
         thisIsANewMethod();
+        // fresh comments in random places
     }
 
     public void thisIsANewMethod(){
         // sus
-	String coolName = "Astolfo";
+	String coolName = "Astolfo";        // fresh comment
         System.out.println("I want a body pillow please");
-        System.out.println("\n");
+        System.out.println("\n");       // freshest comment
     }
 }
